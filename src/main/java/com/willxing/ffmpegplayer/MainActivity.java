e483ee77c72e5fe8e84cb491b2f103e57a7cca51
邢维兴 <saitestop@gmail.com>
2019-08-03 23:03:37 +0800

    add ffmpege head file And add Button on Activity

--- src/main/java/com/willxing/ffmpegplayer/MainActivity.java
+++ src/main/java/com/willxing/ffmpegplayer/MainActivity.java
@@ -1,13 +1,83 @@
 package com.willxing.ffmpegplayer;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    static {
+        System.loadLibrary("ffmpegplayer-lib");
+    }
+    Button btnPlay,btnPasue,btnStop,btnReset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btnplay);
+        btnPasue = findViewById(R.id.btnpause);
+        btnStop = findViewById(R.id.btnstop);
+        btnReset = findViewById(R.id.btnreset);
+
+        btnPlay.setOnClickListener(this);
+        btnPasue.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnplay:
+                playUrl();
+                break;
+            case R.id.btnpause:
+                pause();
+                break;
+            case R.id.btnstop:
+                stop();
+                break;
+            case R.id.btnreset:
+                reset();
+                break;
+                default:
+        }
+    }
+
+    public void playUrl(){
+        final String inputurl  = "http://192.168.1.106:8000/Jupiter.mp4";
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(final SurfaceHolder holder) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        play(inputurl, holder.getSurface());
+                    }
+                }).start();
+
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+            }
+        });
+
+    }
+
+    public native void play(String url, Surface surface);
+    public native void pause();
+    public native void stop();
+    public native void reset();
 }
